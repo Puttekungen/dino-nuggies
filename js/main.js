a3d519f402573590de9f62454a3c6a044ce85c1e
Dwargon <Victor.nordvall@gmail.com>
2025-04-03 11:15:36 +0200

    större

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
     meteor.src = images[currentIndex];
 }, 300);
+
+document.addEventListener("DOMContentLoaded", function () {
+    let gameWindow = document.querySelector(".gamewindow");
+    let position = 0;
+
+    function scrollBackground() {
+        position -= 2; // Ändra hastigheten här
+        gameWindow.style.backgroundPosition = position + "px 0";
+        requestAnimationFrame(scrollBackground);
+    }
+
+    scrollBackground();
+});
