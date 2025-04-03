421980290b2ecbd4b75d9a0a058f7fe1542c7074
Dwargon <Victor.nordvall@gmail.com>
2025-04-03 10:55:42 +0200

    mycket

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+let animatedImage = document.getElementById("animatedImage");
+
+let images = ["../img/meteor1.png", "../img/meteor2.png"];
+let currentIndex = 0;
+
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    meteor.src = images[currentIndex];
+}, 300);
