09e75463aa7aa6a49ba6c0e59d8d4bf1f1ec5d63
LulleBulle0 <lucas.niemimansson@skola.taby.se>
2025-04-03 11:25:34 +0200

    extra kod för meteor

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let animatedImage = document.getElementById("animatedImage");
 let images = ["../img/meteor1.png", "../img/meteor2.png"];
 let currentIndex = 0;
 
+let meteor = document.getElementById("meteor");
+
 setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
     meteor.src = images[currentIndex];
